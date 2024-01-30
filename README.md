c9c9d12d2f274cb8f00e4d52acc3d900333dda4e
seanveal <seand.veal@yahoo.com>
2024-01-30 13:50:55 -0600

    Theme Switcher completed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SwiftUI-App-Theme-Switcher-iOS17
 Switching App theme from light to dark mode
+[Tutorial by Kavsoft](https://youtube.com/watch?v=aHtDymtNdSs)
---
+++ ThemeChanger/ThemeChanger/ThemeChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeChangerApp.swift
+//  ThemeChanger
+//
+//  Created by Sean Veal on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ThemeChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
